e37b520d3f7e46b84cdd69e869c46bc89b537c89
Bhanu Prasanth Konda <32713350+bhanuprasanthkonda@users.noreply.github.com>
2021-04-09 01:25:06 +0530

    Added Live Video for Recognition

---
+++ VideoFeed.py
@@ -0,0 +1,23 @@
+import cv2
+from FaceRecognition import FaceRecognition
+
+
+def liveVideoFeed():
+    cam = cv2.VideoCapture(0)
+    fr = FaceRecognition()
+    try:
+        while True:
+            successFullFrameRead, frame = cam.read()
+            frame = fr.startUp(frame)
+            cv2.imshow("Live Face Detection", frame)
+            if cv2.waitKey(1) != -1:
+                break
+    except Exception as e:
+        print(e)
+    finally:
+        cv2.destroyAllWindows()
+        cam.release()
+
+
+if __name__ == "__main__":
+    liveVideoFeed()
